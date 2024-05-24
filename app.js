b0cab5e214fd36b3129b1b5e524a0670441035eb
semel <semel.yakoub@gmail.com>
2024-05-24 18:42:41 +0300

    add the index page

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 
 const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use("/", require("./routes/pages"));
 
 app.listen(3000, () => {
   console.log("server started on port 3000");
